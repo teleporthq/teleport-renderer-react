7b3fc0dce5615d8eeb8f3363a1a83635ca4c3b5b
CRISTIAN MOLDOVAN <crissmoldovan@gmail.com>
2018-06-30 18:09:11 +0300

    added element renderer

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ElementRenderer_1 = require("./ElementRenderer");
+exports.ElementRenderer = ElementRenderer_1.default;
+//# sourceMappingURL=index.js.map
